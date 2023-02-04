5245ecb530229b2c33315575ad3ce74fdaafeb9a
bari-d1 <dayobomi2004@gmail.com>
2023-02-04 22:08:10 +0000

    Done for today...will continue tomorrow

--- react_gym_app/src/Welcomepage.js
+++ react_gym_app/src/Welcomepage.js
@@ -1,20 +1,21 @@
-import React from "react"
-import ReactDOM from "react-dom"
+import React from "react";
+import ReactDOM from "react-dom";
 import './Welcomepage.css';
-import layer0 from "./images/layer0.png"
+import layer0 from "./images/layer0.png";
+import { FaArrowCircleRight } from 'react-icons/fa';
 
 class Welcomepage extends React.Component {
     render() {
       return(
-        <div className="conatiner">
-            <div className="greeting">
+        <div id="conatiner">
+            <div id="greeting">
                 <h1>Welcome Dayo</h1>
             </div>
-            <div className="imageholder">
+            <div id="imageholder">
                 <img src={layer0} className="image" alt="strong guy" />
             </div>
-            <div className="proceed">
-                <button>Continue</button>
+            <div id="proceed">
+                <div id="next_page">Continue <FaArrowCircleRight fontSize= "2.5em"/></div>
             </div>
         </div>
       ) 
