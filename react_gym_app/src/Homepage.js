3c067eef9333565fd7c7fdf88b15d086db0a3936
Oluwaniayomi Ogundana <oluwaniayomi.ogundana@student.manchester.ac.uk>
2023-02-06 01:33:00 +0000

    Progress bar added

--- react_gym_app/src/Homepage.js
+++ react_gym_app/src/Homepage.js
@@ -5,8 +5,36 @@ import {BiHomeAlt} from "react-icons/bi"
 import {AiOutlineCalendar} from "react-icons/ai"
 import {FaDumbbell} from "react-icons/fa"
 
+let index = 0;
+let exc_list = ["Planks", "Push Ups", "Bench", "Treadmill", "Pull Ups", "Stretches"];
+let percentages = []
+let progress_bar_width = 0.8*414; //This is static rn
+let progress_content = document.getElementById("progress_content");
+// progress_content.style.display = "none";
+let progress_bar = document.getElementById("progress_bar");
+let boxes = [false, false, false, false, false, false];
 
 class Homepage extends React.Component{
+    // constructor(){
+    //     this.current_progress = "0";
+    //     this.exc_list = ["Planks", "Push Ups", "Bench", "Treadmill", "Pull Ups", "Stretches"];
+    // }
+    
+    clicked(){
+        // .style.textDecoration = "line-through";
+        document.getElementById("progress_content").style.display = "block";
+        percentages = []
+        let percent = 0
+        for (let m=0; m<exc_list.length; m++){
+            percent += progress_bar_width/exc_list.length
+            percentages.push(percent)
+        }
+        document.getElementById("progress_content").style.width = percentages[index] + "px"
+        index++
+    }
+
+
+
     render(){
         return (
             <>
@@ -18,26 +46,35 @@ class Homepage extends React.Component{
                         <div id="middle">
                             <h2>Workout B</h2>
                             <section id="options">
-                                <input type="checkbox" id="_1"/>
-                                <label for="_1">Planks</label><br />
-                                <input type="checkbox" id="_2"/>
-                                <label for="_2">Push Ups</label><br />
-                                <input type="checkbox" id="_3"/>
-                                <label for="_3">Bench</label><br />
-                                <input type="checkbox" id="_4"/>
-                                <label for="_4">Treadmill</label><br />
-                                <input type="checkbox" id="_5"/>
-                                <label for="_5">Pull Ups</label><br />
-                                <input type="checkbox" id="_6"/>
-                                <label for="_6">Stretches</label><br />
+                                <button className="box" onClick={this.clicked}>Planks</button>
+                                <button className="box" onClick={this.clicked}>Push Ups</button>
+                                <button className="box" onClick={this.clicked}>Bench</button>
+                                <button className="box" onClick={this.clicked}>Treadmill</button>
+                                <button className="box" onClick={this.clicked}>Pull Ups</button>
+                                <button className="box" onClick={this.clicked}>Stretches</button>
                             </section>
                             {/* Need to add the progress bar */}
+                            <section id="progress">
+                                {/* <svg width="300" height="20">
+                                    <rect id="total" width="300" height="20"></rect>
+                                    <rect id="sofar" width="30" height="20"></rect>
+                                </svg> */}
+                                <div id="progress_bar">
+                                    <div id="progress_content" className="display">
+                
+                                    </div>
+                                </div>
+                            </section>
                         </div>
-                        <div id="bottom">
-                            <button><BiHomeAlt /></button>
+                        <nav id="bottom">
+                            {/* We can use href="target" to link a webpage to itself */}
+                            <li><a href="#"><BiHomeAlt /></a></li> 
+                            <li><a href="#"><AiOutlineCalendar /></a></li>
+                            <li><a href="#"><FaDumbbell /></a></li>
+                            {/* <button><BiHomeAlt /></button>
                             <button><AiOutlineCalendar /></button>
-                            <button><FaDumbbell /></button>
-                        </div>
+                            <button><FaDumbbell /></button> */}
+                        </nav>
                     </div>
                 </div>
             </>
