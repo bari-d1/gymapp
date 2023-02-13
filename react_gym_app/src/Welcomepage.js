019fb013447e20ee9de9ccce659febb9073a8912
bari-d1 <dayobomi2004@gmail.com>
2023-02-13 16:44:54 +0000

    welcome page complete without linking to other pages

--- react_gym_app/src/Welcomepage.js
+++ react_gym_app/src/Welcomepage.js
@@ -4,19 +4,23 @@ import './Welcomepage.css';
 import './style.css';
 import layer0 from "./images/layer0.png";
 import { FaArrowCircleRight } from 'react-icons/fa';
+import { FaDumbbell } from 'react-icons/fa';
+import 'animate.css';
+
 
 class Welcomepage extends React.Component {
     render() {
       return(
-        <div id="container">
+        <div id="container" className="grid-container">
             <div id="greeting">
                 <h1>Welcome Dayo</h1>
             </div>
             <div id="imageholder">
-                <img src={layer0} className="image" alt="strong guy" />
+                <FaDumbbell className="strong animate__bounceInDown"/>
+                {/* <img src={layer0} className="image" alt="strong guy" /> */}
             </div>
             <div id="proceed">
-                <div id="next_page">Continue <FaArrowCircleRight fontSize= "2.5em"/></div>
+                <div id="next_page"><h1>Continue</h1><FaArrowCircleRight className="arrow" fontSize= "2em"/></div>
             </div>
         </div>
       ) 
